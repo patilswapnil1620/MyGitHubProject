951bd5e6082ae34bd65800705ef70aff1e09b6cb
patilswapnil1620 <patilswapnil1620@gmail.com>
2022-09-28 15:37:53 +0530

    greetings

--- force-app/main/default/lwc/welcomeComp/welcomeComp.js
+++ force-app/main/default/lwc/welcomeComp/welcomeComp.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class WelcomeComp extends LightningElement {}
+export default class WelcomeComp extends LightningElement {
+    name = 'Swapnil!!';
+}
